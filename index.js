4cfbbfd3f4743ba076293845ba8025c348eeedda
Breno Calazans <brenocalazans@gmail.com>
2018-01-12 16:43:14 -0200

    Put files in root

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const path = require('path')
 const tachyonsGenerator = require('tachyons-generator')
 const config = require('./config.json')
 
@@ -8,8 +7,8 @@ const generate = async () => {
 
   const out = await tachy.generate()
   
-  fs.writeFileSync(path.join(__dirname, 'docs', 'index.html'), out.docs)
-  fs.writeFileSync(path.join(__dirname, 'docs', 'tachyons.css'), out.css)
+  fs.writeFileSync('index.html', out.docs)
+  fs.writeFileSync('tachyons.css', out.css)
 }
 
 generate()
