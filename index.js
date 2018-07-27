c9bac1243f9fe5b51502627363a873fed2be8cb1
Breno Calazans <brenocalazans@gmail.com>
2018-07-27 12:33:40 -0300

    Add color tokens

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const config = require('./config.json')
 const generate = async () => {
   const tachy = tachyonsGenerator(config)
 
-  const out = await tachy.generate()
+  const docs = await tachy.docs()
+  const css = await tachy.generate()
+  const cssMin = await tachy.generate({ minify: true })
   
-  fs.writeFileSync('index.html', out.docs)
-  fs.writeFileSync('tachyons.css', out.css)
-  fs.writeFileSync('tachyons.min.css', out.min)
+  fs.writeFileSync('index.html', docs)
+  fs.writeFileSync('tachyons.css', css)
+  fs.writeFileSync('tachyons.min.css', cssMin)
 }
 
 generate()
