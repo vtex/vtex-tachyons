0254fd2b3d301c0c5dea78533c5807e84d16b750
Breno Calazans <brenocalazans@gmail.com>
2018-07-27 12:34:01 -0300

    Add scoped version

--- index.js
+++ index.js
@@ -8,10 +8,24 @@ const generate = async () => {
   const docs = await tachy.docs()
   const css = await tachy.generate()
   const cssMin = await tachy.generate({ minify: true })
-  
+
   fs.writeFileSync('index.html', docs)
   fs.writeFileSync('tachyons.css', css)
   fs.writeFileSync('tachyons.min.css', cssMin)
 }
 
 generate()
+
+const generateScoped = async () => {
+  const namespace = 'onda-v1'
+  const scopedConfig = Object.assign({}, { namespace }, config)
+  const tachy = tachyonsGenerator(scopedConfig)
+
+  const css = await tachy.generate()
+  const cssMin = await tachy.generate({ minify: true })
+
+  fs.writeFileSync('tachyons-scoped.css', css)
+  fs.writeFileSync('tachyons-scoped.min.css', cssMin)
+}
+
+generateScoped()
