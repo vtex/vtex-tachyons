d85fb4c9d7489effcff8d4391bc3f01340a761db
Christian Kaisermann <christian@kaisermann.me>
2019-06-11 16:46:03 -0300

    Add print stylesheets generation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+
+- Add print stylesheets: `tachyons.print.css` and ``tachyons.print.min.css`
+
 ## [3.1.2] - 2019-05-16
 
 ### Fixed
@@ -22,6 +26,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [3.1.0] - 2019-03-13
 
 ### Added
+
 - Add position sticky token.
 
 ## [3.0.2] - 2019-02-15
@@ -51,6 +56,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [2.9.0] - 2018-11-19
 
 ### Added
+
 - Viewport width tokens e.g.`.vw-100`
 
 ## [2.8.4] - 2018-11-14
@@ -58,21 +64,25 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [2.8.3] - 2018-11-14
 
 ### Fixed
+
 - Change hover-c-danger color to correct value
 
 ## [2.8.0] - 2018-10-15
 
 ### Added
+
 - Add typography styles tokens `.t-heading-1`, `.t-heading-2`, `.t-heading-3`, `.t-heading-4`, `.t-heading-5`, `.t-heading-6`, `.t-body`, `.t-small`, `.t-mini`, `.t-action`, `.t-action--small`, `.t-action--large` and `.t-code`. All of those classes have their responsive counterparts, i.e., `.t-body-s`, `.t-body-ns`, `.t-body-m`, `.t-body-l` and `.t-body-xl`.
 
 ## [2.7.0] - 2018-08-13
 
 ### Added
+
 - Add `.visited-c-link`, `.hover-c-link`, `.active-c-link` and `.c-link`
 - Add `.active-` classes
 - Add missing `.c-action-primary` and `.c-action-secondary`
 
 ### Fixed
+
 - Fix background colors
 - Fix hover background colors
 - Fix border colors
--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-const fs = require('fs')
+const { writeFile } = require('fs').promises
 const path = require('path')
 const tachyonsGenerator = require('@vtex/tachyons-generator')
 
 const config = require('./config.js')
 
-// Write config file as JSON to keep API compatibility
-fs.writeFileSync(
-  path.join(__dirname, 'config.json'),
-  JSON.stringify(config, null, 2),
-  'utf8'
-)
+const writeTo = (file, promisedContent) =>
+  promisedContent.then(data => writeFile(file, data))
 
-const generate = async () => {
-  const tachy = tachyonsGenerator(config)
+const init = async () => {
+  // Write config file as JSON to keep API compatibility
+  await writeFile(
+    path.join(__dirname, 'config.json'),
+    JSON.stringify(config, null, 2),
+    'utf8',
+  )
 
-  const docs = await tachy.docs()
-  const css = await tachy.generate()
-  const cssMin = await tachy.generate({ minify: true })
+  const generate = () => {
+    const tachy = tachyonsGenerator(config)
 
-  fs.writeFileSync('index.html', docs)
-  fs.writeFileSync('tachyons.css', css)
-  fs.writeFileSync('tachyons.min.css', cssMin)
+    writeTo('index.html', tachy.docs())
+    writeTo('tachyons.css', tachy.generate())
+    writeTo('tachyons.min.css', tachy.generate({ minify: true }))
+    writeTo('tachyons.print.css', tachy.generatePrint({ key: 'print' }))
+    writeTo(
+      'tachyons.print.min.css',
+      tachy.generatePrint({ key: 'print', minify: true }),
+    )
+  }
+
+  generate()
+
+  const generateScoped = () => {
+    const namespace = 'onda-v3'
+    const scopedConfig = Object.assign({}, { namespace }, config)
+    const tachy = tachyonsGenerator(scopedConfig)
+
+    writeTo('tachyons-scoped.css', tachy.generate())
+    writeTo('tachyons-scoped.min.css', tachy.generate({ minify: true }))
+  }
+
+  generateScoped()
 }
 
-generate()
-
-const generateScoped = async () => {
-  const namespace = 'onda-v3'
-  const scopedConfig = Object.assign({}, { namespace }, config)
-  const tachy = tachyonsGenerator(scopedConfig)
-
-  const css = await tachy.generate()
-  const cssMin = await tachy.generate({ minify: true })
-
-  fs.writeFileSync('tachyons-scoped.css', css)
-  fs.writeFileSync('tachyons-scoped.min.css', cssMin)
-}
-
-generateScoped()
+init()
